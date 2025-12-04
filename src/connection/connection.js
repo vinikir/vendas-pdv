645e69ee5b476849c1c7959a624f165376c6e9cd
vinikir <vinikir@yahoo.com.br>
2025-12-04 15:10:11 -0300

    ajuste url

--- src/connection/connection.js
+++ src/connection/connection.js
@@ -4,7 +4,8 @@ const api = axios.create({
    
     //baseURL:"http://18.222.164.74/",
     //baseURL:"https://urvu65w6he.execute-api.us-east-2.amazonaws.com/",
-    baseURL:"http://192.168.0.59:3300/",
+    baseURL:"https://vendasbackcentral-388770734965.us-central1.run.app/",
+    //baseURL:"http://192.168.0.59:3300/",
   
     timeout: 30000,
     headers: {
